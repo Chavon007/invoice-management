ff0606a97eb86e70592674816612ff4bb6b5db1c
azuh salvation <salvationazuh@gmail.com>
2026-04-21 00:25:54 +0100

    feat: add InvoiceContext with CRUD actions and localStorage persistence

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/themeContext.tsx";
+import { InvoiceProvider } from "./context/invoiceContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
